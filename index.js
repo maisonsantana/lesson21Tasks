ee2fc5c55f23824f6dee705e8e80bf00e7d72a0b
eugene <jeka.tomchuk@gmail.com>
2022-12-02 13:48:53 +0200

    Completed tasks

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 // Виправити помилки в коді
 // повинен бути результат:  'To Be Or Not To Be'
 
+const sentence = "     to         BE       oR   nOt        To     bE ";
+function toJadenCase(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .filter(Boolean)
+    .map((word) => word[0].toUpperCase() + word.substring(1))
+    .join(" ");
+}
+console.log(toJadenCase(sentence));
